b9cb2b48f5002bae950d8d024315526e9da0c2e6
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-05-31 02:20:20 -0400

    Remove old dotfile function

--- fish/functions/gdot.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function gdot --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME' --description 'Aliased git command to manage bare repo ~/.dotfiles.git'
-  /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv;
-end
