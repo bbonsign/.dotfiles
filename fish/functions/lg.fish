f05266e14a83246c9e862c00de563eb0bcf0b22b
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-07-08 15:55:11 -0400

    Add aliases for exa, tmux, and neovim

---
+++ fish/functions/lg.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function lg --wraps='exa --all --long --git --group-directories-first --icons' --description 'alias lg exa --all --long --git --group-directories-first --icons'
+  exa --all --long --git --group-directories-first --icons $argv;
+end
