68ff39525cfbf73437bc809262990791b4493b58
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-07-25 16:15:43 -0400

    Alias exa with tree option

---
+++ fish/functions/lt.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function lt --wraps='exa --all --long --group-directories-first --icons --tree' --description 'alias lt exa --all --long --group-directories-first --icons --tree'
+  exa --all --long --group-directories-first --icons --tree $argv;
+end
