866e92703eb34cf48089e10aadca97bfbba0e457
Brian Bonsignore <bonsignore.brian@gmail.com>
2021-01-10 01:12:14 -0500

    Remove alias for python3

--- fish/functions/python.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function python --wraps=usr/local/bin/python3 --wraps=/usr/local/bin/python3 --description 'alias python /usr/local/bin/python3'
-  /usr/local/bin/python3  $argv;
-end
