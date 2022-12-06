88884169caed1e88eb7ae826d2527c8edda91f57
Brian Bonsignore <bbonsign@users.noreply.github.com>
2022-12-06 00:09:45 -0500

    Use vi bindings in fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,29 @@ source ~/.config/fish/colors/fish_tokyonight_storm.fish
 
 
 fzf_configure_bindings --variables=\e\cv --git_log=\e\cg &>/dev/null
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
 
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block
+# Set the insert mode cursor to a line
+set fish_cursor_insert line
+# Set the replace mode cursor to an underscore
+set fish_cursor_replace_one underscore
+# The following variable can be used to configure cursor shape in
+# visual mode, but due to fish_cursor_default, is redundant here
 set fish_cursor_visual block
+set fish_cursor_visual block
+
+set -g fish_escape_delay_ms 10
 
 # bgcolor of the current tab completion selection
 set fish_color_search_match --background=4b719c
